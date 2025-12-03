2b7348ae37f3a8db6149fb05429c241e5cf3d92d
wallajess <wallajess@gmail.com>
2025-12-03 15:23:43 +0100

    successfully ran day1 part2

--- day1/src/Main.java
+++ day1/src/Main.java
@@ -5,32 +5,43 @@ import java.util.List;
 public static void main(String[] args) {
 
         try {
-            List<String> lines = Files.readAllLines(Paths.get("/home/jazz/Documents/adventOfCode/AoC1_test.txt"));
+            List<String> lines = Files.readAllLines(Paths.get("/home/jazz/Documents/adventOfCode/AoC1.txt"));
             String[] directions = lines.toArray(new String[0]);
 
             Dial dial = new Dial(50);
             int numZeros = 0;
 
             for (String instruction : directions) {
+
+                //Remove BOM
                 instruction = instruction.replace("\uFEFF", "").trim();
 
+                //Determine position prior to rotation
                 int oldPos = dial.position;
 
+                //What direction it will turn
+                char direction =  instruction.charAt(0);
+
+                //How many clicks it will make
                 int dist = Integer.parseInt(instruction.substring(1));
 
-                int numRotations =  dist / 100;
-                numZeros += numRotations;
+                //Position after turning
+                dial.rotation(instruction);
 
-                if (dial.rotation(instruction) == 0) {
-                    numZeros++;
-                }
-                if (instruction.charAt(0) == 'R' && ((Integer.parseInt(instruction.substring(1)) % 100) + oldPos) > 99) {
-                    numZeros++;
-                } else if (instruction.charAt(0) == 'L' && ((Integer.parseInt(instruction.substring(1)) %100) - oldPos) < 1) {
-                    numZeros++;
+                //To account for numbers over 100
+                numZeros += dist/100;
+
+                int partialDist = dist % 100;
+
+                if (direction == 'R' && (partialDist + oldPos) > 99) {
+                        numZeros++;
+                    } else if (direction == 'L' && (oldPos - partialDist) < 1) {
+                        if (oldPos != 0) {
+                            numZeros++;
+                        }
+                    }
                 }
 
-            }
             System.out.println("The password is: " + numZeros);
 
         } catch (IOException e) {
